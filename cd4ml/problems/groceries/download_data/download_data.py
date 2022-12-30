95b4affd4caba04576c99f72037403bdf0bdeb5a
Guilherme Silveira <guilherme.silveira@gmail.com>
2022-12-30 12:04:30 -0300

    downloading and unzipping groceries data from course account

--- cd4ml/problems/groceries/download_data/download_data.py
+++ cd4ml/problems/groceries/download_data/download_data.py
@@ -1,9 +1,10 @@
 from cd4ml.filenames import get_problem_files
 from cd4ml.utils.utils import download_to_file_from_url, shuffle_csv_file
+import zipfile
 
-download_params = {'key': 'store47-2016.csv',
-                   'gcs_bucket': 'continuous-intelligence',
-                   'base_url': 'https://storage.googleapis.com'}
+download_params = {'key': 'store47-2016',
+                   'gcs_bucket': 'raw/main',
+                   'base_url': 'https://github.com/alura-cursos/continuous-delivery-for-machine-learning-data/'}
 
 
 def get_grocery_url_and_files(problem_name):
@@ -13,12 +14,15 @@ def get_grocery_url_and_files(problem_name):
     base_url = download_params['base_url']
 
     filename = file_names['raw_grocery_data']
-    url = "%s/%s/%s" % (base_url, gcs_bucket, key)
+    url = "%s/%s/%s.zip" % (base_url, gcs_bucket, key)
     filename_shuffled = file_names['grocery_data_shuffled']
     return url, filename, filename_shuffled
 
 
 def download(problem_name, use_cache=True):
     url, filename, filename_shuffled = get_grocery_url_and_files(problem_name)
-    download_to_file_from_url(url, filename, use_cache=use_cache)
+    zipfilename = f"${filename}.zip"
+    download_to_file_from_url(url, zipfilename, use_cache=use_cache)
+    with zipfile.ZipFile(zipfilename, 'r') as zip_ref:
+        zip_ref.extractall()
     shuffle_csv_file(filename, filename_shuffled)
